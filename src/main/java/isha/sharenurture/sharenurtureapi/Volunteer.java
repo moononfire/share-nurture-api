ff02a94dba021f13d372a0134b0551c916d720d4
shiva <vikbobinski@gmail.com>
2021-11-23 22:37:11 +0100

    getters and setters for Volunteer

--- src/main/java/isha/sharenurture/sharenurtureapi/Volunteer.java
+++ src/main/java/isha/sharenurture/sharenurtureapi/Volunteer.java
@@ -17,4 +17,20 @@ public class Volunteer {
     public Volunteer() {
 
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
