1bb253700bb9825f878c16687b106f7f740325e7
shiva <vikbobinski@gmail.com>
2021-11-23 22:25:32 +0100

    Initial commit

---
+++ src/main/java/isha/sharenurture/sharenurtureapi/ShareNurtureApiApplication.java
@@ -0,0 +1,13 @@
+package isha.sharenurture.sharenurtureapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareNurtureApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareNurtureApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/isha/sharenurture/sharenurtureapi/ShareNurtureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package isha.sharenurture.sharenurtureapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareNurtureApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
