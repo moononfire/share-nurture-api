b397eb8e61b1270dcb136eae1d0b64018ca92a61
shiva <vikbobinski@gmail.com>
2021-11-26 09:16:39 +0100

    import tidy up

--- src/main/java/isha/sharenurture/sharenurtureapi/model/Content.java
+++ src/main/java/isha/sharenurture/sharenurtureapi/model/Content.java
@@ -1,6 +1,5 @@
 package isha.sharenurture.sharenurtureapi.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
